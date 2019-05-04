069ea2195e0257d023cf7c1d2da5dfb38a5878e9
Kumbhar <pooja-prakash.kumbhar@capgemini.com>
2019-05-04 15:58:46 +0530

    files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SongDetailsModule } from './song-details/song-details.module';
+import { AlbumDetailsModule } from './album-details/album-details.module';
+import { LoginComponent } from './login.component';
+import { SignupComponent } from './signup.component';
 
 @NgModule({
   declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent
+  ],
+  bootstrap: [
     AppComponent
   ],
+
   imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    BrowserModule,
+    SongDetailsModule,
+    AlbumDetailsModule
+  ]
+
+  
+ 
 })
 export class AppModule { }
